176004d1cd0c85808ab9369d6b39c69cbb6e2274
Jonathan Cedeño <jony.cede@gmail.com>
2021-04-09 14:11:52 +0200

    place the days of the month in their corresponding box

--- calendar-generate.js
+++ calendar-generate.js
@@ -1,23 +1,29 @@
-var year = 2021;
-var month = 1;  // Month in number format: January -> 0 and December -> 11
+var year = 2025;
+var month = 5;  // Month in number format: January -> 0 and December -> 11
 
 const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-generateCalendar(month);
+generateCalendar(year, month);
 
-function generateCalendar(p_month){
-    p_month++;
-    var daysMonth = new Date(year, p_month, 0).getDate();
+function generateCalendar(p_year, p_month){
+    var daysMonth = new Date(p_year, p_month+1, 0).getDate();
+    var firstDayMonth = new Date(p_year, p_month, 1). getDay();
 
     generateGridEmpty();
     
-    // The next lines insert the week days in the grid
+    // The next lines insert the week days in the grid and the id's in the first line in the grid
     for(var i = 0; i < weekDays.length; i++){
         document.getElementsByClassName("week-day")[i].innerHTML = weekDays[i];
     }
     
+    // The next lines insert the day numbers in the grid according to the month and year
     for (var i = 0; i < daysMonth; i++) {
-        document.getElementsByClassName("grid-day")[i].innerHTML = i+1;
+        if(firstDayMonth == 0){
+            document.getElementsByClassName("grid-day")[i+7-1].innerHTML = i+1;
+        }
+        else{
+            document.getElementsByClassName("grid-day")[i+firstDayMonth-1].innerHTML = i+1;
+        }
     }
 }
 
