7e0967a6d477b7cdbad7db85651f0ab83a50c443
Jonathan Cedeño <jony.cede@gmail.com>
2021-04-09 13:44:04 +0200

    added the day numbers in grid

--- calendar-generate.js
+++ calendar-generate.js
@@ -1,24 +1,34 @@
-var month = 1;
+var year = 2021;
+var month = 1;  // Month in number format: January -> 0 and December -> 11
+
 const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 generateCalendar(month);
 
-function generateCalendar(month){
+function generateCalendar(p_month){
+    p_month++;
+    var daysMonth = new Date(year, p_month, 0).getDate();
+
     generateGridEmpty();
     
-    for(i=0 ; i<weekDays.length ; i++){
+    // The next lines insert the week days in the grid
+    for(var i = 0; i < weekDays.length; i++){
         document.getElementsByClassName("week-day")[i].innerHTML = weekDays[i];
     }
     
+    for (var i = 0; i < daysMonth; i++) {
+        document.getElementsByClassName("grid-day")[i].innerHTML = i+1;
+    }
 }
 
+// This function generates the grid in main
 function generateGridEmpty(){
-    for(i=0 ; i<49 ; i++){
-        if(i<8){
+    for(var i = 0; i < 49; i++){
+        if(i<7){
             document.querySelector("main").insertAdjacentHTML("beforeend", "<div class='week-day'></div>");
         }
         else{
-            document.querySelector("main").insertAdjacentHTML("beforeend", "<div></div>");
+            document.querySelector("main").insertAdjacentHTML("beforeend", "<div class='grid-day'></div>");
         }
     }
 }
