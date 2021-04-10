948ae7cd33b126edfaf67e9ed5b45f08a583b443
jonCroatanUto <jon.garciaorad.asencor@gmail>
2021-04-10 17:26:58 +0200

    botones de abrir y cerrar formulario

--- formulario.js
+++ formulario.js
@@ -1,13 +1,27 @@
 document.getElementById("add-event-button").addEventListener("click",callTemplate);
-
+x=0;
 function callTemplate(){
-    console.log('boton funciona')
-   let newEvent=document.querySelector("template.newEvent");
+   
+    if(x==0){
+        let newEvent=document.querySelector("template.newEvent");
 
-    const importNewEvent=document.importNode(newEvent.content,true);
-    document.querySelector("body").appendChild(importNewEvent);
+        const importNewEvent=document.importNode(newEvent.content,true);
+        document.querySelector("body").appendChild(importNewEvent);
 
-    let eventContent=document.getElementById("eventContent");
-    eventContent.style.border='1px black solid';
+        let eventContent=document.getElementById("eventContent");
+        eventContent.style.border='1px black solid';
+        document.getElementById('close-button').addEventListener('click', closeEvent);
+        document.getElementById('canel-button').addEventListener('click', closeEvent);
+        document.getElementById('create-button').addEventListener('click', closeEvent);
+        x+=1;
+        function closeEvent(){
+            document.querySelector("body").removeChild(eventContent);
+            x=0;
+            };
+        
+    }else{
+        document.querySelector("body").removeChild(eventContent);
+        x=0;
+    }
 
 }
