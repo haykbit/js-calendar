8f4ea7927ded64da039c7f664abc50163f781ad0
jonCroatanUto <jon.garciaorad.asencor@gmail>
2021-04-10 18:43:58 +0200

    habilitar/deshabilitar el recordatorio

--- formulario.js
+++ formulario.js
@@ -1,7 +1,9 @@
 document.getElementById("add-event-button").addEventListener("click",callTemplate);
-x=0;
+x=0;//_______________________contador para que no se llame al formulario dos veces
+
+
 function callTemplate(){
-   
+
     if(x==0){
         let newEvent=document.querySelector("template.newEvent");
 
@@ -10,18 +12,39 @@ function callTemplate(){
 
         let eventContent=document.getElementById("eventContent");
         eventContent.style.border='1px black solid';
-        document.getElementById('close-button').addEventListener('click', closeEvent);
-        document.getElementById('canel-button').addEventListener('click', closeEvent);
-        document.getElementById('create-button').addEventListener('click', closeEvent);
-        x+=1;
-        function closeEvent(){
-            document.querySelector("body").removeChild(eventContent);
-            x=0;
+    //___________________________________________________recoger datos del formulario
+
+    //_____________________________________________________________cerrar formulario
+            document.getElementById('close-button').addEventListener('click', closeEvent);
+            document.getElementById('cancel-button').addEventListener('click', closeEvent);
+            document.getElementById('create-button').addEventListener('click', sendAndCloseEvent);
+            x+=1;
+            function closeEvent(){
+                document.querySelector("body").removeChild(eventContent);
+                x=0;
             };
-        
+            function sendAndCloseEvent(){
+                document.querySelector("body").removeChild(eventContent);
+                x=0;
+            }
     }else{
+    //________________cambio la funcion del boton "add-event-button" par que envez de 
+    //__________________ abrir otro formulario cierre el que ya esta abierto
         document.querySelector("body").removeChild(eventContent);
         x=0;
     }
 
+}
+//____________________________________________________ habilitar los recordatorios
+function habilitar(remind){
+    let renindImputs=document.querySelectorAll(".habitiation");
+    if(remind.checked==true){
+       renindImputs.forEach(element => {
+           element.removeAttribute("disabled")
+        });
+    }else{
+        renindImputs.forEach(element => {
+            element.setAttribute("disabled","")
+         });
+    }
 }
